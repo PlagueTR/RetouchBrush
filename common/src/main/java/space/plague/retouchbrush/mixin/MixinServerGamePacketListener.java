bfe28da949d89d92f658bfce20f9b321a9d82946
PlagueTR <116841534+PlagueTR@users.noreply.github.com>
2026-06-21 13:33:37 +0300

    Switch to Mixins - Kept Cooldown for Vanilla

---
+++ common/src/main/java/space/plague/retouchbrush/mixin/MixinServerGamePacketListener.java
@@ -0,0 +1,98 @@
+package space.plague.retouchbrush.mixin;
+
+import net.minecraft.network.protocol.game.ServerboundInteractPacket;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.server.network.ServerGamePacketListenerImpl;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.decoration.Painting;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.GameType;
+import net.minecraft.world.phys.Vec3;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import space.plague.retouchbrush.Main;
+import space.plague.retouchbrush.config.ModConfig;
+import space.plague.retouchbrush.util.PaintingCycleUtil;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+@Mixin(ServerGamePacketListenerImpl.class)
+public class MixinServerGamePacketListener {
+
+    @Shadow
+    public ServerPlayer player;
+
+    private static final Map<UUID, Long> LAST_INTERACTION_TICK = new HashMap<>();
+
+    @Inject(method = "handleInteract(Lnet/minecraft/network/protocol/game/ServerboundInteractPacket;)V", at = @At("HEAD"), cancellable = true)
+    private void handleInteract(ServerboundInteractPacket packet, CallbackInfo ci) {
+        packet.dispatch(new ServerboundInteractPacket.Handler() {
+            @Override
+            public void onInteraction(InteractionHand hand) {
+                Entity entity = packet.getTarget(player.serverLevel());
+
+                if (!(entity instanceof Painting painting) || !player.getItemInHand(hand).is(Items.BRUSH)) {
+                    return;
+                }
+
+                UUID playerUUID = player.getUUID();
+                long currentTick = player.level().getGameTime();
+
+                if (LAST_INTERACTION_TICK.containsKey(playerUUID)) {
+                    long lastTick = LAST_INTERACTION_TICK.get(playerUUID);
+                    if (currentTick - lastTick < 1) {
+                        ci.cancel();
+                        return;
+                    }
+                }
+
+                ModConfig config = Main.getConfig();
+                if (!config.isEnableMod() || !config.isEnableRightClickBehavior()) {
+                    return;
+                }
+
+                GameType gamemode = player.gameMode.getGameModeForPlayer();
+                if (gamemode == GameType.SPECTATOR){
+                    return;
+                }
+
+                PaintingCycleUtil.CycleTo mode;
+                try {
+                    mode = PaintingCycleUtil.CycleTo.valueOf(config.getCycleTo());
+                } catch (IllegalArgumentException e) {
+                    config.setCycleTo(PaintingCycleUtil.CycleTo.NEXT.getCode());
+                    mode = PaintingCycleUtil.CycleTo.NEXT;
+                }
+
+                boolean success = PaintingCycleUtil.cyclePainting(painting, player.level(), mode, config.isKeepSize());
+                if (!success) {
+                    ci.cancel();
+                    return;
+                }
+
+                LAST_INTERACTION_TICK.put(playerUUID, currentTick);
+
+                if (!player.isCreative() && config.isEnableUseDamage()) {
+                    player.getItemInHand(hand).hurtAndBreak(1, player, p -> p.broadcastBreakEvent(hand));
+                }
+                ci.cancel();
+
+            }
+
+            @Override
+            public void onInteraction(InteractionHand interactionHand, Vec3 vec3) { }
+
+            @Override
+            public void onAttack() { }
+        });
+    }
+
+}
--- fabric/src/main/java/space/plague/retouchbrush/fabric/MainFabric.java
+++ fabric/src/main/java/space/plague/retouchbrush/fabric/MainFabric.java
@@ -3,13 +3,10 @@ package space.plague.retouchbrush.fabric;
 import net.fabricmc.api.ModInitializer;
 
 import space.plague.retouchbrush.Main;
-import space.plague.retouchbrush.fabric.registry.RightClickBehavior;
 
 public final class MainFabric implements ModInitializer {
     @Override
     public void onInitialize() {
-
-        RightClickBehavior.register();
 
         Main.init();
 
